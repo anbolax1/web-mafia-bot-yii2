8ceefbdd6b9faaeea6542a59d51e7d03f4351f40
Andrey <andrej.bockow@yandex.ru>
2023-08-22 20:41:35 +0300

    правки конфига нгинкс

--- config/db.php
+++ config/db.php
@@ -1,10 +1,16 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
+    /*'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=db;dbname=web-mafia-bot',
     'username' => 'istwood',
     'password' => 'istwood',
+    'charset' => 'utf8',*/
+
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=web-mafia-bot-yii2',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
