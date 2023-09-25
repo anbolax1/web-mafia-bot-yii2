9a09661a44b1571ce839efcb4bb9d89e20fe7363
andrey <andrej.bockow@yandex.ru>
2023-09-25 20:57:53 +0300

    правки стилей профиля

--- views/site/profile.php
+++ views/site/profile.php
@@ -10,7 +10,7 @@ GameAsset::register($this);
     <div class="col-md-7" style="height: 8em;width: auto;display: flex;flex-wrap: wrap;align-content: space-around;">
         <img style="width: 7em; height: 7em; border-radius: 50%;" src="<?= "https://cdn.discordapp.com/avatars/$discordUser->discord_id/$discordUser->avatar.jpg"; ?>" alt="Avatar" class="avatar">
     </div>
-    <div class="col-md-1" style="height: 8em;width: 10vw;display: flex;flex-wrap: wrap;justify-content: space-between;">
+    <div class="col-md-1" style="height: 8em;min-width: 11em;max-width: 15%;display: flex;flex-wrap: wrap;justify-content: space-between;">
         <p style="display: flex;flex-wrap: wrap;justify-content: space-between;width:100%;margin:0;font-size: 1.7em;color: whitesmoke;font-weight: bold;"><?=$discordUser->username ?></p>
         <p style="display: flex;flex-wrap: wrap;justify-content: space-between;width:100%;color: whitesmoke;margin: 0;"><span>Игр сыграно:</span><span><?=$gamesPlayedCount ?></span></p>
         <p style="display: flex;flex-wrap: wrap;justify-content: space-between;width:100%;color: whitesmoke;margin: 0;"><span>Игр выиграно:</span><span><?=$gamesWonCount ?></span></p>
